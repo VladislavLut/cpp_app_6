5215eba8810d433eb62f89d39c3a4ef5cc24c4c5
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-19 10:39:32 +0300

    Initial commit

--- main.cpp
+++ main.cpp
@@ -6,12 +6,5 @@ int main()
 {
 	system("chcp 1251");
 	system("cls");
-	int year;
-
-	cout << "Введіть номер року: ";
-	cin >> year;
-	bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-	int daysinYear = 365 + isLeapYear;
-	cout << " У " << year << " році = " << daysinYear << " днів " << endl;
-	return 0;
+	
 }
