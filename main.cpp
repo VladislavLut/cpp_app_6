56d6057d1cb34b5fa89e22be3e6fc684396f84d8
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-19 17:42:09 +0300

    ex-5 was done

--- main.cpp
+++ main.cpp
@@ -3,47 +3,19 @@
 #include <cmath>
 using namespace std;
 
-const double EarthRadiusKm = 0371.0;
-
-double toRadians(double degress) {
-	return degress * 3,14 / 100;
-}
-double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-
-	lat1 = toRadians(lat1);
-	lon1 = toRadians(lon1);
-	lat2 = toRadians(lat2);
-	lon2 = toRadians(lon2);
-
-	double dlat = lat2 - lat1;
-	double dlon = lon2 - lon1;
-
-	double a = sin(dlat / 2) * sin(dlat / 2) + cos(lat1) * cos(lat2) * sin(dlon / 2) * sin(dlon / 2);
-	double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-
-	double distance = EarthRadiusKm * c;
-	return distance;
-
-}
+const double Pi = 3.14159265358979323846;
 
 int main()
 {
 	system("chcp 1251");
 	system("cls");
-	double lat1, lon1, lat2, lon2;
-	cout << "¬вед≥ть широту першого населеного пункту градуси:";
-	cin >> lat1;
-	cout << "¬вед≥ть довготу першого населеного пункту градуси: ";
-	cin >> lon1;
-	cout << "¬вед≥ть широту другого населеного пункту градуси: ";
-	cin >> lat2;
-	cout << "¬вед≥ть довготу другого населеного пункту градуси: ";
-	cin >> lon2;
+	double radius, volume;
+	cout << "¬вед≥ть рад≥ус кул≥ у сантиметрах: ";
+	cin >> radius;
 
-	double distance = calculateDistance(lat1, lon1, lat2, lon2);
-	cout << "¬≥дстань м≥ж населеними пунктами: " << distance << " к≥лометр≥в" << endl;
+	volume = (4.0 / 3.0) * Pi * pow(radius, 3);
+
+	cout << "ќб'Їм кул≥: " << volume << " куб≥чних сантиметр≥в" << endl;
 
 	return 0;
-
-	
 }
