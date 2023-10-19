b10b0660d615e5c307446f20de9336b0886caf2c
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-19 17:07:57 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -6,14 +6,5 @@ int main()
 {
 	system("chcp 1251");
 	system("cls");
-	double lenght, width, height, volume;
-	cout << "¬вед≥ть довжину паралелеп≥педа у сантиметрах: ";
-	cin >> lenght;
-	cout << "¬вед≥ть ширину паралелеп≥педа у сантиметрах: ";
-	cin >> width;
-	cout << "¬вед≥ть висоту паралелеп≥педа у сантиметрах: ";
-	cin >> height;
-	volume = lenght * width * height;
-	cout << "ќб'Їм паралелеп≥педа: " << volume << " куб≥чних сантиметр≥в " << endl;
-	return 0;
+	
 }
