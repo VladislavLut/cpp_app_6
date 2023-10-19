1de5271f50e353c6bf3e91a0dbe4f5122887ed3e
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-19 10:52:36 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -6,13 +6,5 @@ int main()
 {
 	system("chcp 1251");
 	system("cls");
-	int grivna, kopiyka;
-	cout << "Введіть гривні: ";
-	cin >> grivna;
-	cout << "Введіть копійки: ";
-	cin >> kopiyka;
-	grivna += kopiyka / 100;
-	kopiyka = kopiyka % 100;
-	cout << "Правильна сумма: " << grivna << " грн. " << kopiyka << " коп. " << endl;
-	return 0;
+	
 }
